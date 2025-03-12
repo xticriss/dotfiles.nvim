fd35dc2d51e287d7db3f81fbd51366718bee5043
Cristian Lascu <cristian.lascu@instantfactoring.com>
2025-03-12 16:40:46 +0000

    - added avante diffview gitsigns and harpoon

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,67 +1,68 @@
 return {
-	"yetone/avante.nvim",
-	event = "VeryLazy",
-	lazy = false,
-  build = "make BUILD_FROM_SOURCE=true",
-	opts = {
-		-- add any opts here
-	},
-	keys = {
-		{
-			"<leader>aa",
-			function()
-				require("avante.api").ask()
-			end,
-			desc = "avante: ask",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>ar",
-			function()
-				require("avante.api").refresh()
-			end,
-			desc = "avante: refresh",
-		},
-		{
-			"<leader>ae",
-			function()
-				require("avante.api").edit()
-			end,
-			desc = "avante: edit",
-			mode = "v",
-		},
-	},
-	dependencies = {
-		"stevearc/dressing.nvim",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		--- The below dependencies are optional,
-		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-		"zbirenbaum/copilot.lua", -- for providers='copilot'
-		{
-			-- support for image pasting
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				-- recommended settings
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					-- required for Windows users
-					use_absolute_path = true,
-				},
-			},
-		},
-		{
-			-- Make sure to setup it properly if you have lazy=true
-			"MeanderingProgrammer/render-markdown.nvim",
-			opts = {
-				file_types = { "markdown", "Avante" },
-			},
-			ft = { "markdown", "Avante" },
-		},
-	},
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    provider = "claude",
+    auto_suggestions_provider = "claude",
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "o3-mini", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- timeout in milliseconds
+      temperature = 0, -- adjust if needed
+      max_tokens = 4096,
+    },
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-5-sonnet-20241022", -- claude-3-5-sonnet-20241022 -- claude-3-7-sonnet-20250219
+      temperature = 0,
+      max_tokens = 4096,
+    },
+    mappings = {
+      sidebar = {
+        switch_windows = "<C-Tab>",
+      }
+    }
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
 }
